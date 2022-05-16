e8060908843636050e9e3208e8c7531774ea0c20
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-16 15:46:36 -0700

    isediting conditional

--- src/views/Entry.jsx
+++ src/views/Entry.jsx
@@ -9,16 +9,33 @@ export default function Entry() {
   const { id } = useParams();
   const [entry, setEntry] = useState({});
   const { entries, getEntry } = useEntries();
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     setEntry(getEntry(id));
   }, [id, entries.length]);
+
+  const HandleEditButton = () => {};
+
+  let content;
+
+  if (isEditing) {
+    content = (
+      <form>
+        <input type="text" name="title" value={entry?.title} />
+        <input type="date" name="date" value={entry?.date} />
+        <textarea name="content" value={entry?.content} />
+        <button></button>
+      </form>
+    );
+  }
 
   return (
     <>
       <Link to="/entries" className={styles.backButton}>
         &laquo; Back to Planner
       </Link>
+      <button onClick={HandleEditButton}>{isEditing ? 'Save' : 'Edit'}</button>
       <article className={styles.entry}>
         <h1>{entry?.title}</h1>
         <p>Due: {entry?.date}</p>
