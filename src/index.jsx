3022c82517dfdf2fd96576a5dc958c146c097b2f
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-16 14:35:38 -0700

    plannercontext added to index js

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { PlannerContext } from './context/PlannerContext';
 
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <PlannerContext.Provider
+    value={{
+      entries,
+      addEntry,
+      getEntry,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </PlannerContext.Provider>,
   document.getElementById('root')
 );
